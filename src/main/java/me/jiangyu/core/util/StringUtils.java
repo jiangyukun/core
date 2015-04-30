1485c1eb06d43aa8648fc36a8d9a637d694f8e53
jiangyukun <187_6810_5877@sina.com>
2015-04-30 12:41:44 +0800

    Initial commit

---
+++ src/main/java/me/jiangyu/core/util/StringUtils.java
@@ -0,0 +1,11 @@
+package me.jiangyu.core.util;
+
+/**
+ * 字符串工具类
+ * Created by jiangyukun on 2015/4/30.
+ */
+public class StringUtils {
+    public static boolean isEmpty(String str) {
+        return str != null && str.length() != 0;
+    }
+}
